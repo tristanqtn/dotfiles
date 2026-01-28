f4228a79bd221542d0e553ab8ffb41e494c75f08
tristanqtn <tristan.querton@gmail.com>
2026-01-28 23:55:18 +0100

    feat: add VsCode extensions

--- home/vscode/default.nix
+++ home/vscode/default.nix
@@ -1,12 +1,21 @@
-{pkgs, ...}:
+{ pkgs, ... }:
 {
   programs.vscode = {
     enable = true;
     package = pkgs.vscode.fhs;
 
     extensions = with pkgs.vscode-extensions; [
+      # Nix support
       bbenoist.nix
-    ];    
+
+      # Docker support
+      docker.docker
+
+      # Material icons
+      pkief.material-icon-theme
+
+      esbenp.prettier-vscode     # Code formatting
+      ms-python.python           # Python support if you code in Python
+    ];
   };
-} 
-  
+}
