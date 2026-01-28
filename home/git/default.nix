4db08b9ec45bf2bb95a1a68d23995cf38ff05c37
Tristan <tristan.querton@gmail.com>
2026-01-28 19:15:08 +0100

    feat: add basic tools

---
+++ home/git/default.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }:
+{
+  programs.git = {
+    enable = true;
+    package = pkgs.gitFull;
+
+    settings = { 
+      user = {
+        name  = "tristanqtn";
+        email = "tristan.querton@gmail.com";
+      };
+      init.defaultBranch = "main";
+      pull.rebase = true;
+    };
+
+  };
+}
